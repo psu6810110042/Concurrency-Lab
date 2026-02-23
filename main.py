b5096451c692cf69e109dc253d8cc020da4ae890
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-02-23 13:10:26 +0700

    ADDED: ping command and example for how to use it

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from concurrency-project!")
+import subprocess
+import platform
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def ping_ip(ip):
+    # -n for windows -c for linux and other os
+    param = "-n" if platform.system().lower() == "windows" else "-c"
+    command = ["ping", param, "1", ip]
+
+    try:
+        result = subprocess.run(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1
+        )
+
+        # returns 0 for success
+        if result.returncode == 0:
+            return f"[+] ONLINE: {ip}"
+        else:
+            return None
+
+    except subprocess.TimeoutExpired:
+        return None
 
 
 if __name__ == "__main__":
-    main()
+    print(ping_ip("172.31.201.112"))
