c43095015f321350f5f6898f3b9c28fa2396f4a0
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-02-23 13:18:59 +0700

    ADDED: sequential pinging for the ips and examples

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 
+# function for pinging devices
 def ping_ip(ip):
     # -n for windows -c for linux and other os
     param = "-n" if platform.system().lower() == "windows" else "-c"
@@ -24,5 +25,24 @@ def ping_ip(ip):
         return None
 
 
+# sequential ping
+def run_sequential_ping(ips):
+    print(f"\n--- Starting Sequential Ping Scan on {len(ips)} IPs ---")
+    start_time = time.perf_counter()
+
+    for ip in ips:
+        result = ping_ip(ip)
+        if result:
+            print(result)
+
+    end_time = time.perf_counter()
+    print(f"Sequential Scan took: {end_time - start_time:.2f} seconds")
+
+
 if __name__ == "__main__":
-    print(ping_ip("172.31.201.112"))
+    # print(ping_ip("172.31.201.112"))
+    BASE_IP = "172.31.201."
+
+    # scan ips from 1-254
+    ips_to_scan = [f"{BASE_IP}{i}" for i in range(1, 255)]
+    run_sequential_ping(ips_to_scan)
